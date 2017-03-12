90f8789d6f187687d38e4b25ffaca2fccf5cd03c
Roman <greenfield0000@mail.ru>
2017-03-12 19:13:41 +0300

    Create brunch ncs

---
+++ src/main/java/controller/HemmingNetUiController.java
@@ -0,0 +1,479 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.*;
+import javafx.scene.control.SingleSelectionModel;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import net.HemmingNet;
+import utils.FileUtil;
+
+import javax.swing.*;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.*;
+
+import static javafx.scene.control.ScrollPane.ScrollBarPolicy.ALWAYS;
+
+public class HemmingNetUiController implements Initializable {
+    public AnchorPane initializationTab;
+    public TabPane mainPane;
+    public Tab trainingPane;
+    public HBox viewSimplesPane;
+    public AnchorPane viewMainSimplesPane;
+    public ScrollPane viewFindSimple;
+    public ScrollPane viewFoundSimple;
+    public Tab weightingCoefficientMatrixView;
+    public Tab scalesFeedbackMatrixView;
+    public AnchorPane weightingCoefficientMatrix;
+    public AnchorPane scalesFeedbackMatrixMatrix;
+    public AnchorPane graphic;
+    public Tab graphicTab;
+    private HemmingNet net;
+    SingleSelectionModel<Tab> selectionModel;
+    List<Integer> coefficient;
+    List<List<Integer>> coefficientList = new ArrayList<>();
+    int numberBinarySigns;
+    int numberNeurons;
+
+    @FXML
+    public TextField numberNeuronsField;
+    @FXML
+    public TextField numberBinarySignsField;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        net = new HemmingNet();
+        selectionModel = mainPane.getSelectionModel();
+        coefficient = new ArrayList<>();
+    }
+
+    @FXML
+    private void initializationNetAction() {
+        if (numberBinarySignsField.getText().isEmpty() || numberNeuronsField.getText().isEmpty()) {
+            new Alert(Alert.AlertType.ERROR, "Не заполнены входные данные !").show();
+            return;
+        }
+        numberBinarySigns = Integer.parseInt(numberBinarySignsField.getText());
+        numberNeurons = Integer.parseInt(numberNeuronsField.getText());
+        net.setNumberBinarySigns((int) Math.pow(numberBinarySigns, 2));
+        net.setNumberNeurons(numberNeurons);
+        initializationTab.setDisable(true);
+        trainingPane.setDisable(false);
+        selectionModel.select(1);
+    }
+
+    /**
+     * Добавление нового образа
+     */
+    public void addSimpleAction() {
+        if (viewSimplesPane.getChildren().size() < numberNeurons) {
+            createDialog();
+        } else {
+            new Alert(Alert.AlertType.INFORMATION, "Добавлено максимальное количество образцов");
+        }
+    }
+
+    /**
+     * Добавление нового образа "Диалоговое окно" при простой инициализации
+     */
+    private void createDialog() {
+        Stage dialog = new Stage();
+        VBox box = new VBox();
+        box.setAlignment(Pos.CENTER);
+        Control input = createInputPane(numberBinarySigns);
+        Button buttonOk = new Button("Ok");
+        buttonOk.setOnAction(event -> {
+            net.addReferenceSample(new ArrayList<>(coefficient));
+            dialog.close();
+            input.getChildrenUnmodifiable().stream().forEach(it -> it.setDisable(true));
+            viewSimplesPane.getChildren().add(input);
+        });
+        Button buttonEx = new Button("Close");
+        buttonEx.setOnAction(evt -> dialog.close());
+        HBox buttons = new HBox();
+        buttons.setAlignment(Pos.CENTER);
+        buttons.getChildren().addAll(buttonOk, buttonEx);
+        box.getChildren().addAll(input, buttons);
+        Scene scene = new Scene(box, (numberBinarySigns * 40) + 100, (numberBinarySigns * 40) + 100);
+        dialog.setScene(scene);
+        dialog.show();
+    }
+
+
+    private Control createInputPane(int numberBinarySigns) {
+        coefficient.clear();
+        int columns, rows;
+        columns = rows = numberBinarySigns;
+        GridPane grid = new GridPane();
+        grid.getStyleClass().add("game-grid");
+
+        for (int i = 0; i < columns; i++) {
+            ColumnConstraints column = new ColumnConstraints(42);
+            grid.getColumnConstraints().add(column);
+        }
+
+        for (int i = 0; i < rows; i++) {
+            RowConstraints row = new RowConstraints(42);
+            grid.getRowConstraints().add(row);
+        }
+
+        int count = -1;
+        for (int i = 0; i < columns; i++) {
+            for (int j = 0; j < rows; j++) {
+                Pane pane = new Pane();
+                ++count;
+                int finalCount = count;
+                pane.setOnMouseReleased(event -> {
+                    if (event.getButton().equals(MouseButton.SECONDARY)) {
+                        coefficient.set(finalCount, -1);
+                        pane.getChildren().add(getRectangle(Color.WHITE));
+                    }
+                    if (event.getButton().equals(MouseButton.PRIMARY)) {
+                        coefficient.set(finalCount, 1);
+                        pane.getChildren().add(getRectangle(Color.BLACK));
+                    }
+                });
+                coefficient.add(-1);
+                pane.getChildren().add(getRectangle(Color.WHITE));
+                pane.getStyleClass().add("game-grid-cell");
+                if (j == 0) {
+                    pane.getStyleClass().add("first-column");
+                }
+                if (i == 0) {
+                    pane.getStyleClass().add("first-row");
+                }
+                grid.add(pane, j, i);
+            }
+        }
+        coefficientList.add(coefficient);
+        ScrollPane sp = new ScrollPane();
+        sp.setContent(grid);
+        sp.setHbarPolicy(ALWAYS);
+        sp.setVbarPolicy(ALWAYS);
+        return sp;
+    }
+
+    /**
+     * Закрашиваем цветом квадратик
+     * @param color
+     * @return
+     */
+    private static Node getRectangle(Color color) {
+        Rectangle rectangle = new Rectangle(1, 1, 39, 39);
+        rectangle.setFill(color);
+        return new Group(rectangle);
+    }
+
+    /**
+     * Удаление образа
+     */
+    public void deletedSimpleAction() {
+        Stage dialog = new Stage();
+        VBox box = new VBox();
+        box.setAlignment(Pos.CENTER);
+        TextField input = new TextField();
+        Button buttonOk = new Button("Ok");
+        buttonOk.setOnAction(event -> {
+            int index = Integer.parseInt(input.getText());
+            dialog.close();
+            viewSimplesPane.getChildren().remove(index);
+            net.getReferenceSamplesMatrix().remove(index);
+        });
+        Button buttonEx = new Button("Close");
+        buttonEx.setOnAction(evt -> dialog.close());
+        HBox buttons = new HBox();
+        buttons.setAlignment(Pos.CENTER);
+        buttons.getChildren().addAll(buttonOk, buttonEx);
+        box.getChildren().addAll(new Label("Введите индекс"), input, buttons);
+        Scene scene = new Scene(box, 100, 100);
+        dialog.setScene(scene);
+        dialog.show();
+    }
+
+    /**
+     * Обучение сети.
+     */
+    public void teachingNetAction() {
+        if (viewSimplesPane.getChildren().size() == numberNeurons) {
+            net.teachTheNetwork();
+            //trainingPane.setDisable(true);
+            selectionModel.select(2);
+            selectionModel.getSelectedItem().setDisable(false);
+            weightingCoefficientMatrixView.setDisable(false);
+            scalesFeedbackMatrixView.setDisable(false);
+            viewMainSimplesPane.getChildren().stream().forEach(it -> {
+                if (it instanceof Button) {
+                    it.setDisable(true);
+                }
+            });
+            completeTableWeightingCoefficient();
+            completeTableScalesFeedbackMatrix();
+        } else {
+            new Alert(Alert.AlertType.INFORMATION, "Добавлены не все образцы");
+        }
+    }
+
+    private void completeTableScalesFeedbackMatrix() {
+        GridPane grid = new GridPane();
+        grid.getStyleClass().add("game-grid");
+
+        for (int i = 0; i < numberNeurons; i++) {
+            ColumnConstraints column = new ColumnConstraints(42);
+            grid.getColumnConstraints().add(column);
+        }
+
+        for (int i = 0; i < numberNeurons; i++) {
+            RowConstraints row = new RowConstraints(42);
+            grid.getRowConstraints().add(row);
+        }
+
+        List<List<Double>> scalesFeedback = net.getScalesFeedbackMatrix();
+        List<Double> scales;
+        for (int i = 0; i < numberNeurons; i++) {
+            scales = scalesFeedback.get(i);
+            for (int j = 0; j < scales.size(); j++) {
+                Pane pane = new Pane();
+                Label label = new Label(String.format("%.2f", scales.get(j)));
+                label.setLayoutY(10);
+                label.setLayoutX(10);
+                pane.getChildren().add(label);
+                pane.getStyleClass().add("game-grid-cell");
+                if (j == 0) {
+                    pane.getStyleClass().add("first-column");
+                }
+                if (i == 0) {
+                    pane.getStyleClass().add("first-row");
+                }
+                grid.add(pane, j, i);
+            }
+        }
+        ScrollPane sp = new ScrollPane();
+        sp.setContent(grid);
+        sp.setHbarPolicy(ALWAYS);
+        sp.setVbarPolicy(ALWAYS);
+        scalesFeedbackMatrixMatrix.getChildren().add(sp);
+    }
+
+    private void completeTableWeightingCoefficient() {
+        GridPane grid = new GridPane();
+        grid.getStyleClass().add("game-grid");
+
+        for (int i = 0; i < Math.pow(numberBinarySigns, 2); i++) {
+            ColumnConstraints column = new ColumnConstraints(42);
+            grid.getColumnConstraints().add(column);
+        }
+
+        for (int i = 0; i < numberNeurons; i++) {
+            RowConstraints row = new RowConstraints(42);
+            grid.getRowConstraints().add(row);
+        }
+
+        List<List<Double>> weightingCoefficient = net.getWeightingCoefficientMatrix();
+        List<Double> weighting;
+        for (int i = 0; i < numberNeurons; i++) {
+            weighting = weightingCoefficient.get(i);
+            for (int j = 0; j < weighting.size(); j++) {
+                Pane pane = new Pane();
+                Label label = new Label(weighting.get(j).toString());
+                label.setLayoutY(10);
+                label.setLayoutX(10);
+                pane.getChildren().add(label);
+                pane.getStyleClass().add("game-grid-cell");
+                if (j == 0) {
+                    pane.getStyleClass().add("first-column");
+                }
+                if (i == 0) {
+                    pane.getStyleClass().add("first-row");
+                }
+                grid.add(pane, j, i);
+            }
+        }
+        ScrollPane sp = new ScrollPane();
+        sp.setContent(grid);
+        sp.setHbarPolicy(ALWAYS);
+        sp.setVbarPolicy(ALWAYS);
+        weightingCoefficientMatrix.getChildren().add(sp);
+    }
+
+    /**
+     * Распознать
+     */
+    public void runNetAction() {
+        Integer resultVector = net.runNet();
+        if (resultVector == -1) {
+            new Alert(Alert.AlertType.INFORMATION, "Извините, не могу определить").show();
+        } else {
+            viewFoundSimple.setContent(createOutputPane(net.getReferenceSamplesMatrix().get(resultVector)));
+        }
+        graphicTab.setDisable(false);
+        createGraphic();
+    }
+
+    /**
+     * Построение графика
+     */
+    private void createGraphic() {
+        graphic.getChildren().clear();
+        NumberAxis xAxis = new NumberAxis();
+        xAxis.setLabel("Номер итерации");
+        NumberAxis yAxis = new NumberAxis();
+        yAxis.setLabel("Ошибка на данной итерации");
+        //creating the chart
+        LineChart<Number, Number> lineChart =
+                new LineChart<>(xAxis, yAxis);
+
+        lineChart.setTitle("График сходимости сети");
+        //defining a series
+        XYChart.Series<Number, Number> series = new XYChart.Series<>();
+        series.setName("Ошибка");
+        //populating the series with data
+        int index = 0;
+        for (Double error : net.getErrors()) {
+            ++index;
+            series.getData().add(new XYChart.Data<>(index, error));
+        }
+
+        lineChart.getData().add(series);
+        graphic.getChildren().add(lineChart);
+    }
+
+    /**
+     * Построение
+     * @param outputVector
+     * @return
+     */
+    private Node createOutputPane(List<Integer> outputVector) {
+        coefficient.clear();
+        int columns, rows;
+        columns = rows = numberBinarySigns;
+        GridPane grid = new GridPane();
+        grid.getStyleClass().add("game-grid");
+
+        for (int i = 0; i < columns; i++) {
+            ColumnConstraints column = new ColumnConstraints(42);
+            grid.getColumnConstraints().add(column);
+        }
+
+        for (int i = 0; i < rows; i++) {
+            RowConstraints row = new RowConstraints(42);
+            grid.getRowConstraints().add(row);
+        }
+
+        int count = -1;
+        for (int i = 0; i < columns; i++) {
+            for (int j = 0; j < rows; j++) {
+                Pane pane = new Pane();
+                ++count;
+                pane.getChildren().add(outputVector.get(count) == -1 ? getRectangle(Color.WHITE) : getRectangle(Color.BLACK));
+                pane.getStyleClass().add("game-grid-cell");
+                if (j == 0) {
+                    pane.getStyleClass().add("first-column");
+                }
+                if (i == 0) {
+                    pane.getStyleClass().add("first-row");
+                }
+                grid.add(pane, j, i);
+            }
+        }
+        grid.setDisable(true);
+        return grid;
+    }
+
+    /**
+     * Распознай !!!
+     */
+    public void sendFindSimpleAction() {
+        viewFoundSimple.setContent(null);
+        Stage dialog = new Stage();
+        VBox box = new VBox();
+        box.setAlignment(Pos.CENTER);
+        Control input = createInputPane(numberBinarySigns);
+        Button buttonOk = new Button("Ok");
+        buttonOk.setOnAction(event -> {
+            net.submitInputVector(new ArrayList<>(coefficient));
+            dialog.close();
+            input.setDisable(true);
+            viewFindSimple.setContent(input);
+        });
+        Button buttonEx = new Button("Close");
+        buttonEx.setOnAction(evt -> dialog.close());
+        HBox buttons = new HBox();
+        buttons.setAlignment(Pos.CENTER);
+        buttons.getChildren().addAll(buttonOk, buttonEx);
+        box.getChildren().addAll(input, buttons);
+        Scene scene = new Scene(box, (numberBinarySigns * 40) + 100, (numberBinarySigns * 40) + 100);
+        dialog.setScene(scene);
+        dialog.show();
+    }
+
+    /**
+     * Завершение работы
+     *
+     * @param actionEvent
+     */
+    public void exitProg(ActionEvent actionEvent) {
+        System.exit(0);
+    }
+
+    /**
+     * Загрузить данные из файла
+     *
+     * @param actionEvent
+     */
+    public void loadFormFile(ActionEvent actionEvent) throws IOException, ParserConfigurationException {
+        FileUtil fileUtil = new FileUtil();
+        HashMap<String, Object> resMap = (HashMap<String, Object>) fileUtil.loadFile("NeuroNetDataStruct.xml");
+        prepareDataFromLoadFile(resMap);
+    }
+
+    /**
+     * Передача загруженных данных в сеть
+     * @param resMap - подгруженные данные
+     */
+    private void prepareDataFromLoadFile(HashMap<String, Object> resMap) {
+        // если произошла ошибка загрузки, то ничего не делаем
+        if (resMap.get("isSuccess").equals(false)) return;
+
+        numberNeuronsField.setText((String) resMap.get("numberNeuronsField"));
+        numberBinarySignsField.setText((String) resMap.get("numberBinarySignsField"));
+
+        coefficientList = (List<List<Integer>>) resMap.get("coefficientList");
+
+        // функция добавления вектора связей.
+//        net.addReferenceSample();
+
+    }
+
+    /**
+     * Сохранить в файл образы
+     * @param actionEvent
+     * @throws ParserConfigurationException
+     */
+    public void saveToFile(ActionEvent actionEvent) throws ParserConfigurationException {
+
+        FileUtil fileUtil = new FileUtil();
+        try {
+            fileUtil.saveFile("NeuroNetDataStruct.xml", numberNeuronsField.getText(),
+                    numberBinarySignsField.getText(), coefficientList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/utils/FileUtil.java
@@ -0,0 +1,105 @@
+package utils;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.swing.*;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FileUtil {
+
+    private String fileName;
+    private JFileChooser fileChooser;
+    private DocumentBuilderFactory factory;
+    private Document document;
+
+    public FileUtil() throws ParserConfigurationException {
+        factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(true);
+        document = factory.newDocumentBuilder().newDocument();
+    }
+
+
+    /**
+     * @param numberNeuronsField     - count neuron
+     * @param numberBinarySignsField - count binarySignsField
+     * @param coefficientList        - all Images
+     * @throws IOException
+     */
+    public void saveFile(String fileName, String numberNeuronsField, String numberBinarySignsField,
+                         List<List<Integer>> coefficientList) throws IOException {
+        File file = new File("/home/roman/Загрузки/RSATU2016-NCS-master/src/main/resources/" + fileName);
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+
+        try {
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "true");
+            transformer.transform(
+                    new DOMSource(formDocument(numberNeuronsField, numberBinarySignsField, coefficientList)),
+                    new StreamResult(file));
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private Document formDocument(String numberNeuronsField, String numberBinarySignsField,
+                                  List<List<Integer>> coefficientList) {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(true);
+        try {
+            document = factory.newDocumentBuilder().newDocument();
+
+            Element root = document.createElement("root");
+            root.setAttribute("xmlns", "http://www.javacore.ru/schemas/");
+            document.appendChild(root);
+
+            Element numberNeuronsFieldElem = document.createElement("numberNeuronsField");
+            numberNeuronsFieldElem.setAttribute("value", numberNeuronsField);
+            root.appendChild(numberNeuronsFieldElem);
+
+            Element numberBinarySignsFieldElem = document.createElement("numberBinarySignsFieldElem");
+            numberBinarySignsFieldElem.setAttribute("value", numberBinarySignsField);
+            root.appendChild(numberBinarySignsFieldElem);
+
+            Element imagesElem = document.createElement("Images");
+            root.appendChild(imagesElem);
+
+            for (int i = 0; i < coefficientList.size(); i++) {
+                Element imageElem = document.createElement("Image" + (i + 1));
+                imagesElem.appendChild(imageElem);
+
+                List<Integer> coefficient = coefficientList.get(i);
+                for (int j = 0; j < coefficient.size(); j++) {
+                    Element coef = document.createElement("coef");
+                    coef.setAttribute("value", String.valueOf(coefficient.get(j)));
+                    imageElem.appendChild(coef);
+                }
+            }
+
+
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+
+        return document;
+    }
+
+    public Map<String, Object> loadFile(String pathFile) {
+        return new HashMap<>();
+    }
+}
